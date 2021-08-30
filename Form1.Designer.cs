4f1a8fa0503be29386e412e53bc4c7ea5e94d94d
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-08-30 19:07:12 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace Car_Race
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trofeu = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.road2 = new System.Windows.Forms.PictureBox();
+            this.road1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trofeu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 16.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(52, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "     Distanta:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(110, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 16.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(199, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "00";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.trofeu);
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Controls.Add(this.car2);
+            this.panel1.Controls.Add(this.car1);
+            this.panel1.Controls.Add(this.road2);
+            this.panel1.Controls.Add(this.road1);
+            this.panel1.Location = new System.Drawing.Point(31, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(320, 321);
+            this.panel1.TabIndex = 4;
+            // 
+            // trofeu
+            // 
+            this.trofeu.Image = global::Car_Race.Properties.Resources.Primuloc;
+            this.trofeu.Location = new System.Drawing.Point(63, 90);
+            this.trofeu.Name = "trofeu";
+            this.trofeu.Size = new System.Drawing.Size(179, 89);
+            this.trofeu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trofeu.TabIndex = 5;
+            this.trofeu.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Car_Race.Properties.Resources.Player;
+            this.player.Location = new System.Drawing.Point(118, 205);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(54, 89);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.Image = global::Car_Race.Properties.Resources.Car2;
+            this.car2.Location = new System.Drawing.Point(192, 185);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(54, 89);
+            this.car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car2.TabIndex = 3;
+            this.car2.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.Image = global::Car_Race.Properties.Resources.Car1;
+            this.car1.Location = new System.Drawing.Point(63, 26);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(54, 89);
+            this.car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car1.TabIndex = 2;
+            this.car1.TabStop = false;
+            // 
+            // road2
+            // 
+            this.road2.Image = global::Car_Race.Properties.Resources.Road1;
+            this.road2.Location = new System.Drawing.Point(-3, -222);
+            this.road2.Name = "road2";
+            this.road2.Size = new System.Drawing.Size(320, 319);
+            this.road2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.road2.TabIndex = 1;
+            this.road2.TabStop = false;
+            // 
+            // road1
+            // 
+            this.road1.Image = global::Car_Race.Properties.Resources.Road1;
+            this.road1.Location = new System.Drawing.Point(-2, -638);
+            this.road1.Name = "road1";
+            this.road1.Size = new System.Drawing.Size(320, 319);
+            this.road1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.road1.TabIndex = 0;
+            this.road1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 648);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Racing Car";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trofeu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.PictureBox road2;
+        private System.Windows.Forms.PictureBox road1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox trofeu;
+    }
+}
+
